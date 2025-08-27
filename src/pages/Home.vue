86131cdf8ffcfd7f7ef70a6c4a31f0982d472746
Lelovics László <99196324+Laloka2000@users.noreply.github.com>
2025-08-27 15:39:48 +0200

    Added Tailwind CSS to the project

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div style="padding:2rem; background:lightyellow;">
-    <h1>🏠 Home Page Test</h1>
-    <p>If you see this, routing works ✅</p>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">
+      Tailwind is working!
+    </h1>
+    <p class="mt-4 text-lg text-gray-700">Welcome to the Home page</p>
+    <RouterLink to="/projects" class="text-blue-500 underline">
+      Go to Projects
+    </RouterLink>
   </div>
 </template>
 
