d49132b1c53df8e10aa59197f76f60af476ef4ed
Lelovics László <99196324+Laloka2000@users.noreply.github.com>
2025-09-29 10:27:47 +0200

    Fixed some bugs and appearance issues

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="min-h-[calc(80vh-12rem)]  bg-gray-50 dark:bg-gray-900 py-25">
+  <section class="min-h-[calc(80vh-12rem)]  bg-gray-50 dark:bg-gray-900 py-16">
     <div class="max-w-6xl mx-auto px-6">
       <!-- Title -->
       <h2 class="text-4xl font-bold text-center text-white mb-12">Szakmai tapasztalat és tanulmányok 💼</h2>
       <div class="flex flex-col space-y-10">
         <!-- Work Experience -->
-        <div class="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-12">
+        <div class="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-6">
           <h3 class="text-2xl font-semibold text-white">Szoftver és alkalmazás fejlesztő</h3>
           <p class="text-sm text-gray-400">2024 - 2025 | ITZen Kft</p>
           <ul class="mt-2 text-gray-300 list-disc list-inside space-y-1">
@@ -19,12 +19,12 @@
         </div>
 
         <!-- Studies -->
-        <div class="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-12">
+        <div class="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-6">
           <h3 class="text-2xl font-semibold text-white">Programtervező informatikus (FOSZK)</h3>
           <p class="text-sm text-gray-400">2021 - 2024 | Neumann János Egyetem</p>
           <ul class="mt-2 text-gray-300 list-disc list-inside space-y-1">
             <li>Tantárgyak: <span class="text-blue-400">Programozás, Webfejleszés, Adatbázisok, Szoftvertechnológiák,
-                Mobil fejlesztés, Algoritmusok és adatstruktúrák</span></li>
+                Mobilalkalmazás fejlesztés, Algoritmusok és adatstruktúrák</span></li>
             <li>Programozási nyelvek: <span class="text-blue-400">Java, C++, C#, JavaScript, PHP</span></li>
           </ul>
           <br>
